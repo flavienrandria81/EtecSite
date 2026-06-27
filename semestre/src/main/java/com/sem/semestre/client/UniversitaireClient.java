f1b2df1d1f66f19f2492a81fe9870bfc4c7e6a1f
flavienrandria81 <flavienrandria81@gmail.com>
2026-06-27 20:17:08 +0200

    voila

--- semestre/src/main/java/com/sem/semestre/client/UniversitaireClient.java
+++ semestre/src/main/java/com/sem/semestre/client/UniversitaireClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "UNIVERSITAIRE")
+@FeignClient(name = "UNIVESITAIRE")
 public interface UniversitaireClient {
 
     @GetMapping("/api/anneesUniv/{id}")
