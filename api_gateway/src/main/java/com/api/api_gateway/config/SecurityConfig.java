5560a081eb1bfb3c199d497239115a7758b82120
Miaritsoa RANDRIANTSIMBAZAFY <randriantsimbazafymiaritsoa@gmail.com>
2026-07-08 12:08:05 +0300

    uo

--- api_gateway/src/main/java/com/api/api_gateway/config/SecurityConfig.java
+++ api_gateway/src/main/java/com/api/api_gateway/config/SecurityConfig.java
@@ -61,18 +61,4 @@ public class SecurityConfig {
                 .build();
     }
 
-    // CORS FRONTEND
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "Cache-Control"));
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
 }
